7129486bae1129be0355c5d421a4b87804a46978
Vitaly Okhrimenko <vitys96@mail.ru>
2019-12-25 19:44:25 +0300

    first commit

---
+++ MapPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapPlaces
+//
+//  Created by TOOK on 25.12.2019.
+//  Copyright © 2019 TOOK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
